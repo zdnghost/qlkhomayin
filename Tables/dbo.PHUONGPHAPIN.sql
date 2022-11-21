8ea88e532263435c8577ded346a25ff3a40a3b86
zdnghost <75208153+zdnghost@users.noreply.github.com>
2022-11-21 16:19:03 +0700

    database

---
+++ Tables/dbo.PHUONGPHAPIN.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PHUONGPHAPIN] (
+  [PP] [tinyint] NOT NULL,
+  [PHUONGPHAP] [nvarchar](50) NOT NULL,
+  CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED ([PP])
+)
+ON [PRIMARY]
+GO
